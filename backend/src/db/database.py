63247109f8924653658ae7c139a880030617a1c1
Berto . <timeo.berthault@epita.fr>
2026-06-07 16:43:14 +0200

    fix: changed CORS to allow all origins

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
